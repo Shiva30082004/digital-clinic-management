9e9745ca48a637c363eeebe861bce934618d2fc5
Shiva30082004 <shivbhavsar03@gmail.com>
2025-11-30 00:23:41 -0600

    Add patient CRUD API and Patient type

--- src/types/Patient.ts
+++ src/types/Patient.ts
@@ -1,12 +1,12 @@
-type Patient = {
-  patientId: number;
-  firstName: string;
-  lastName: string;
-  emailAddress: string;
-  gender: "M" | "F";
-  dateOfBirth: string;
-  age: number;
-  clinicId: number;
-};
+import { RowDataPacket } from "mysql2";
 
-export default Patient;
+export default interface Patient extends RowDataPacket {
+  patientId: number;              
+  firstName: string;
+  lastName: string;        
+  emailAddress: string;    
+  gender: "M" | "F";
+  dateOfBirth: string;    
+  age: number;                  
+  clinicId: number;
+}
